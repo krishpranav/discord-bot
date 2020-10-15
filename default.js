afa678764ef671f0196fb56201ecda737bf7bfe6
krishpranav <krisna.pranav@gmail.com>
2020-10-15 14:19:35 +0530

    done changes

--- default.js
+++ default.js
@@ -1,10 +1,13 @@
 //discord bot
+//tool name: discordbot
+//language used: javascript
+
 const Discord = require('discord.js')
 const client = new Discord.client();
 
 client.on('message', message => {
 	if (message.content.startWith('ping')){
-		message.reply('pong!');
+		message.reply('pong');
 	}
 });
 
