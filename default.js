8f4877292fa59145554b7c0b6f4112e374e5a79c
krishpranav <krisna.pranav@gmail.com>
2020-10-26 18:25:27 +0530

    done changes

--- default.js
+++ default.js
@@ -1,14 +1,14 @@
 //discord bot
-//language used: javascript
+//used language: javascript
 
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const client = new Discord.client();
 
 client.on('message', message => {
-	if (message.content.startWtih('ping')){
+	if (message.content.startWith('ping')){
 		message.reply('pong!');
 	}
-})
+});
 
 client.on('ready', () => {
 	console.log('I am ready!');
